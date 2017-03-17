2d177f7cb003df75b34244421e346ee713552fd2
Byron <byronblocker@gmail.com>
2017-03-16 17:45:42 -0700

    add Constructor for contact and addresses

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,9 @@ function Contact (first, last){
   this.lastName = last;
   this.addresses = [];
 }
+
+function Address(street, city, state){
+  this.street = street;
+  this.city = city;
+  this.state = state;
+}
